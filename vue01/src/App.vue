68b66a167f0a741ef41a0f3f501ed3df7d7510de
D.seok <ehskstkrjsa@naver.com>
2025-06-01 18:54:13 +0900

    Vue01

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div>
+    <img
+      src="./assets/room0.jpg"
+      alt=""
+    />
+    <p>강남구 원룸</p>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <img
+      src="./assets/room1.jpg"
+      alt=""
+    />
+    <p>강남구 원룸</p>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <img
+      src="./assets/room2.jpg"
+      alt=""
+    />
+    <p>강남구 원룸</p>
+    <p>70 만원</p>
+  </div>
 </template>
 <script>
 export default {
