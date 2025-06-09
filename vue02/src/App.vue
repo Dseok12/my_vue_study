5aab9259b0d338524286fb88f49941990e7afe61
D.seok <ehskstkrjsa@naver.com>
2025-06-09 15:06:06 +0900

    props, component 복습

---
+++ vue02/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="MasterAppWrapper">
+    <Header :메뉴들="부모메뉴들" />
+    <div class="MasterAppInner">
+      <h1>Welcome to My Vue App</h1>
+      <p>This is a simple Vue.js application.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+  name: 'App',
+  data() {
+      return {
+          부모메뉴들: [
+              { name: 'Home', link: 'javascript:;' },
+              { name: 'About', link: 'javascript:;' },
+              { name: 'Random', link: 'javascript:;' },
+          ]
+      }
+  },
+  components: {
+    Header
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ vue02/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="MainMenu--wrapper">
+        <header>
+            <nav class="MainMenu--wrap">
+                <ul class="MainMenu--box">
+                    <li v-for="(메뉴, idx) in 메뉴들" :key="idx" class="MainMenu--item">
+                        <a :href="메뉴.link">
+                            {{ 메뉴.name }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script>
+import '../css/components/Header.css';
+export default {
+    name: 'Header',
+    props: {
+        메뉴들: {type: Array}
+    }
+}
+</script>
