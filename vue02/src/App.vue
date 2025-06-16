8914e0eb29eba9914ab028d3553df0fffddbb0e7
D.seok <ehskstkrjsa@naver.com>
2025-06-16 15:45:33 +0900

    ItemList 컴포넌트, props 추가

--- vue02/src/App.vue
+++ vue02/src/App.vue
@@ -2,14 +2,20 @@
   <div class="MasterAppWrapper">
     <Header :메뉴들="부모메뉴들" />
     <div class="MasterAppInner">
-      <h1>Welcome to My Vue App</h1>
-      <p>This is a simple Vue.js application.</p>
+      <div class="MasterAppContent">
+        <h1>원룸 정보</h1>
+        <ul class="room-list">
+          <Itemlist :원룸들프롭스="원룸들" />
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Itemlist from './components/Itemlist.vue';
+import { oneroom } from './assets/oneroom.js';
 export default {
   name: 'App',
   data() {
@@ -18,11 +24,13 @@ export default {
               { name: 'Home', link: 'javascript:;' },
               { name: 'About', link: 'javascript:;' },
               { name: 'Random', link: 'javascript:;' },
-          ]
+          ],
+          원룸들: oneroom,
       }
   },
   components: {
-    Header
+    Header,
+    Itemlist
   }
 }
 
