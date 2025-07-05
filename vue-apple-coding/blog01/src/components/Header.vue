28022517b7e671ff8b5d7e9415cca230a56cfd96
Dseok <ehskstkrjsa@naver.com>
2025-07-05 18:58:54 +0900

    vue ch02

---
+++ vue-apple-coding/blog01/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">블로그</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">글목록</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">상세페이지</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">글목록</a></li>
+              <li><a class="dropdown-item" href="#">상세페이지</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="검색하세요" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
---
+++ vue-apple-coding/blog01/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './_Master.scss';
+import App from './App.vue';
+import 'bootstrap';
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+createApp(App).mount('#app')
