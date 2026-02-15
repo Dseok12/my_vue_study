6eb99ea584cf65d2ed69ef32904354b52ea54076
Dseok <ehskstkrjsa@naver.com>
2026-02-15 14:49:39 +0900

    탭 만들기 & 탭으로 사진 업로드 페이지 만들기 보기

---
+++ vue03_company/src/components/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post">
+      <div class="post-header">
+        <div class="profile"></div>
+        <span class="profile-name">{{게시물.name}}</span>
+      </div>
+      <div class="post-body" :style="{ backgroundImage: `url(${게시물.postImage})` }"></div>
+      <div class="post-content">
+        <p>{{ 게시물.likes }} Likes</p>
+        <p><strong>{{ 게시물.name }}</strong> {{ 게시물.content }}</p>
+        <p class="date">{{ 게시물.date }}</p>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+
+  props: {
+    게시물: {
+      type: Object,
+      required: true,
+    },
+  },
+  
+}
+</script>
+<style>
+  @import "../css/post.css";
+</style>
