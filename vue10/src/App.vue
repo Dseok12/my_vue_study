e76c1c36c21236b7af54d0c93fd4f13d69f923dd
Dseok <ehskstkrjsa@naver.com>
2026-09-06 22:24:54 +0900

    연습연습

---
+++ vue10/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TheHeader/>
+    <TheView/>
+  </div>
+</template>
+
+<script setup>
+import TheHeader from './layouts/TheHeader.vue';
+import TheView from './layouts/TheView.vue';
+</script>
---
+++ vue10/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    여기는 어바웃.
+    <button @click="fncGoHome">홈으로 이동</button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const route= useRouter();
+
+const fncGoHome = () => {
+  // console.log(router);
+  // console.log(route.path);
+  // console.log(route.name);
+  // console.log(route.params);
+  // console.log(route.query);
+
+  route.push('/');
+};
+
+</script>
