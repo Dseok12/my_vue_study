91cb13607b10593bb16515680607ff9b50419ee3
Dseok <ehskstkrjsa@naver.com>
2026-09-06 22:26:06 +0900

    연습연습

--- vue10/src/App.vue
+++ vue10/src/App.vue
@@ -6,6 +6,9 @@
 </template>
 
 <script setup>
+/*
+  * 이 프로젝트는 매번 다 지웠다가 다시 만들고 있음. 그래서 App.vue는 그냥 이렇게 두고 있음.
+*/
 import TheHeader from './layouts/TheHeader.vue';
 import TheView from './layouts/TheView.vue';
 </script>
